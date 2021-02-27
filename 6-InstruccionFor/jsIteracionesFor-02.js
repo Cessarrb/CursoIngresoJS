724b8427f4b61e997ea9c7eeb69fc5c0e2225f83
unknown <Cessarrb>
2021-02-27 18:25:30 -0300

    2021

--- 6-InstruccionFor/jsIteracionesFor-02.js
+++ 6-InstruccionFor/jsIteracionesFor-02.js
@@ -1,10 +1,221 @@
 //Algañaras Cesar
-function mostrar()
+/*Realizar el algoritmo que permita ingresar 5 paises:
+el continente (validar entre america , asia , europa ,africa y oceania) 
+el nombre del país, 
+cantidad de habitantes en millones entre 1 y 7000 (validar)
+el nivel de pobresa entre (pobre, rico , muy rico) en europa no hay paises pobre(validar)   
+la temperaruta mínima que se registra en su territorio(entre -50 y 50)  
+a)La cantidad de temperaturas pares.
+b)la cantidad de temperaturas impares de europa
+c)El nombre del pais con menos habitantes
+d)la cantidad de paises que superan los 40 grados.
+e)la cantidad de paises de america que tienen menos de 0 grados .
+f)el promedio de habitantes entre los paises ingresados .
+g)el promedio de habitantes entre los paises que superan los 40 grados        
+H) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura. 
+i) que continente tiene mas habitantes.*/
+function mostrar() 
 {
-	let contador;
 
-	for(contador=10;contador>0;contador--)
+	let continenteIngresado;
+	let nombrePaisIngresado;
+	let cantidadHabitantesIngresados;
+	let nivelPobrezaIngresado;
+	let temperaturaMinimaIngresada;
+	let paises;
+	let temperaturasPares;
+	let temperaturaImparEuropa;
+	let nombrePaisMenosHabitantes;
+	let paisMenosHabitantes;
+	let contadorPaisesTemperaturaMasCuarenta;
+	let contadorPaisesTemperaturaMenosCero
+	let SumacantidadHabitantesIngresados;
+	let promedioCantidadHabitantes;
+	let sumaCantidadHabitantesTemperaturaMasCuarenta;
+	let mensaje;
+	let banderaTemperaturaMasBaja;
+	let temperaturaMasBaja;
+	let PaisTemperaturaMasBaja;
+	let sumaHabitantesAmerica;
+	let sumaHabitantesAsia;
+	let sumaHabitantesEuropa;
+	let sumaHabitantesAfrica;
+	let sumaHabitantesOceania;
+
+	sumaHabitantesAmerica =0;
+	sumaHabitantesAsia =0;
+	sumaHabitantesEuropa = 0;
+	sumaHabitantesAfrica = 0;
+	sumaHabitantesOceania = 0;
+	banderaTemperaturaMasBaja = 0;
+	sumaCantidadHabitantesTemperaturaMasCuarenta = 0;
+	SumacantidadHabitantesIngresados = 0;
+	contadorPaisesTemperaturaMenosCero = 0;
+	contadorPaisesTemperaturaMasCuarenta = 0;
+	banderaPaisMenosHabitantes = 0;
+	temperaturaImparEuropa = 0;
+	temperaturasPares = 0;
+	paises = 0;
+
+
+	while (paises < 1) 
 	{
-		console.log(contador)
+		continenteIngresado = prompt("Ingrese el continente");
+		while (isNaN(continenteIngresado) == false || continenteIngresado != "america" && continenteIngresado != "asia" && continenteIngresado != "europa" && continenteIngresado != "africa" && continenteIngresado != "oceania") {
+			continenteIngresado = prompt("Error, ingrese un continente correcto: america, asia, europa, africa y oceania")
+		}
+		nombrePaisIngresado = prompt("Ingrese el nombre del pais");
+		while (isNaN(nombrePaisIngresado) == false) 
+		{
+			nombrePaisIngresado = prompt("Error, ingrese el nombre de pais correcto");
+		}
+		cantidadHabitantesIngresados = prompt("Ingrese la cantidad de habitantes en millones entre 1 y 7000");
+		cantidadHabitantesIngresados = parseInt(cantidadHabitantesIngresados);
+		while (isNaN(cantidadHabitantesIngresados) == true || cantidadHabitantesIngresados < 1 || cantidadHabitantesIngresados > 7000) 
+		{
+			cantidadHabitantesIngresados = prompt("Error, ingrese la cantidad de habitantes correcta entre 1 y 7000");
+			cantidadHabitantesIngresados = parseInt(cantidadHabitantesIngresados);
+		}
+		nivelPobrezaIngresado = prompt("Ingrese el nivel de pobreza: pobre, rico, muy rico");
+		if (continenteIngresado == "europa") 
+		{
+			while (isNaN(nivelPobrezaIngresado) == false || nivelPobrezaIngresado == "pobre") 
+			{
+				nivelPobrezaIngresado = prompt("Error, ingrese el nivel de pobreza: rico o muy rico");
+			}
+		}
+		else {
+			while (isNaN(nivelPobrezaIngresado) == false || nivelPobrezaIngresado != "pobre" && nivelPobrezaIngresado != "rico" && nivelPobrezaIngresado != "muy rico")
+			 {
+				nivelPobrezaIngresado = prompt("Error, Ingrese el nivel de pobreza: pobre, rico, muy rico");
+			}
+		}
+		temperaturaMinimaIngresada = prompt("Ingrese la temperatura minima del territorio (entra -50 y 50");
+		temperaturaMinimaIngresada = parseInt(temperaturaMinimaIngresada);
+		while(isNaN(temperaturaMinimaIngresada)==true || temperaturaMinimaIngresada <-50 || temperaturaMinimaIngresada >50)
+		{
+			temperaturaMinimaIngresada = prompt("Ingrese la temperatura minima del territorio (entra -50 y 50");
+			temperaturaMinimaIngresada = parseInt(temperaturaMinimaIngresada);
+		}
+
+		//Temperaturas Pares
+		if(temperaturaMinimaIngresada%2==0)
+		{
+			temperaturasPares++;
+		}
+
+		//Nombre del pais con menos habitantes
+		if(banderaPaisMenosHabitantes==0 || cantidadHabitantesIngresados<paisMenosHabitantes)
+		{
+			paisMenosHabitantes = cantidadHabitantesIngresados;
+			nombrePaisMenosHabitantes = nombrePaisIngresado;
+			banderaPaisMenosHabitantes++;
+		}
+
+		//)Cantidad de paises que superan los 40 grados.
+		if(temperaturaMinimaIngresada>40)
+		{
+			contadorPaisesTemperaturaMasCuarenta++;
+			sumaCantidadHabitantesTemperaturaMasCuarenta = sumaCantidadHabitantesTemperaturaMasCuarenta + cantidadHabitantesIngresados;
+		}
+
+		//la cantidad de paises de america que tienen menos de 0 grados.
+
+		if(banderaTemperaturaMasBaja==0 || temperaturaMinimaIngresada<temperaturaMasBaja)
+		{
+			temperaturaMasBaja = temperaturaMinimaIngresada;
+			PaisTemperaturaMasBaja = nombrePaisIngresado;
+			banderaTemperaturaMasBaja++;
+		}
+
+
+		SumacantidadHabitantesIngresados = SumacantidadHabitantesIngresados + cantidadHabitantesIngresados;
+
+		switch(continenteIngresado)
+		{
+			case "america":
+				sumaHabitantesAmerica = sumaHabitantesAmerica + cantidadHabitantesIngresados;
+				if(temperaturaMinimaIngresada<1) //la cantidad de paises de america que tienen menos de 0 grados.
+				{
+					contadorPaisesTemperaturaMenosCero++;
+				}
+				break;
+			case "asia":
+				sumaHabitantesAsia = sumaHabitantesAsia + cantidadHabitantesIngresados;
+					break;
+			case "europa":
+				sumaHabitantesEuropa = sumaHabitantesEuropa + cantidadHabitantesIngresados;
+				if(temperaturaMinimaIngresada%2==1) //Temperaturas Impares en Europa
+				{
+					temperaturaImparEuropa++;
+				}
+				break;
+			case "africa":
+				sumaHabitantesAfrica = sumaHabitantesAfrica + cantidadHabitantesIngresados;
+				break;
+			case "oceania":
+				sumaHabitantesOceania = sumaHabitantesOceania + cantidadHabitantesIngresados;
+				break;
+		}
+
+		paises++;
+	}//Fin del while
+
+
+	//el promedio de habitantes entre los paises ingresados
+	promedioCantidadHabitantes = cantidadHabitantesIngresados / 5;
+
+	//el promedio de habitantes entre los paises que superan los 40 grados
+	if(contadorPaisesTemperaturaMasCuarenta>0)
+	{
+		promedioCantidadHabitantesTemperaturaMasCuarenta = sumaCantidadHabitantesTemperaturaMasCuarenta / contadorPaisesTemperaturaMasCuarenta;
 	}
+
+	if(sumaHabitantesAmerica>sumaHabitantesAsia && sumaHabitantesAmerica>sumaHabitantesEuropa && sumaHabitantesAmerica>sumaHabitantesAfrica && sumaHabitantesAmerica>sumaHabitantesOceania)
+	{
+		continenteMayorHabitantes = "america";
+	}
+	else
+	{
+		if(sumaHabitantesAsia>sumaHabitantesEuropa && sumaHabitantesAsia>sumaHabitantesAfrica && sumaHabitantesAsia>sumaHabitantesOceania)
+		{
+			continenteMayorHabitantes = "asia";
+		}
+		else
+		{
+			if(sumaHabitantesEuropa>sumaHabitantesAfrica && sumaHabitantesEuropa>sumaHabitantesOceania)
+			{
+				continenteMayorHabitantes= "europa";
+			}
+			else
+			{
+				if(sumaHabitantesAfrica>sumaHabitantesOceania)
+				{
+					continenteMayorHabitantes = "africa";
+				}
+				else
+				{
+					continenteMayorHabitantes = "oceania";
+				}
+			}
+		}
+	}
+
+	document.write(`La cantidad de temperaturas pares es ${temperaturasPares}`);
+	document.write(`<br>El numero temperaturas impares en europa es ${temperaturaImparEuropa}`);
+	document.write(`<br>El nombre del pais con menos habitantes es ${paisMenosHabitantes}`);
+	document.write(`<br>La cantidad de paises que superan los 40 grados es ${sumaCantidadHabitantesTemperaturaMasCuarenta}`);
+	document.write(`<br>La cantidad de paises America que tienen menos de 0 grados es ${contadorPaisesTemperaturaMenosCero}`);
+	document.write(`<br>El promedio de habitantes entre los paises ingresados es ${promedioCantidadHabitantes}`);
+	if(contadorPaisesTemperaturaMasCuarenta>0)
+	{
+		document.write(`<br>El promedio de habitantes entre los paises que superan 40 grados es ${promedioCantidadHabitantesTemperaturaMasCuarenta}`);
+	}
+	else
+	{
+		mensaje = "No hay pases que superen los 40 grados de temperatura";
+	}
+	document.write(`<br>La temperatura minima ingresada es ${temperaturaMasBaja} y el nombre del pais es ${PaisTemperaturaMasBaja}`);
+	document.write(`<br>El continente que tiene mas habitantes es ${continenteMayorHabitantes}`);
+
 }
--- 6-InstruccionFor/jsIteracionesFor-03.js
+++ 6-InstruccionFor/jsIteracionesFor-03.js
@@ -1,8 +1,244 @@
 //Algañaras Cesar
+/*Para un censo municipal debemos cargar los datos de TODAS las mascotas de nuestra veterinaria
+necesitan saber si es un gato o un perrro u "otra cosa",(solo hay esos tres tipos)
+el tipo de pelaje (corto, largo , sin pelo),
+la edad de la mascota (edad validada);
+el nombre (una palabra)
+la raza  (una palabra) 
+el peso (peso validado) 
+el estadoClinico (enfermo,internado o adopcion)
+la temperaruta corporal
+
+y nos piden informar solo si existe
+a)El perro mas pesado
+b)El porcentaje de enfermos sobre el total de mascotas
+c)El nombre de la ultima mascota de tipo "otra cosa"
+d)El animal sin pelo con menor temperatura corporal
+e)Que tipo de mascota(gato o un perrro u "otra cosa") que tiene el mayor promedio de temperatura corporal
+f)Sumando gatos y perros que porcentaje del total de mascotas son?
+g)Que estado clinico tiene la menor cantidad de mascotas       
+H)Cual es el promedio de kilos de peso de tomando todas las mascotas 
+i)El nombre y raza del gato sin pelos mas liviano*/
 function mostrar()
 {
+	let tipoMascota;
+	let tipoPelajeMascota;
+	let edadMascota;
+	let nombreMascota;
+	let razaMascota;
+	let pesoMascota;
+	let estadoclinicoMascota;
+	let temperaturaCorporalMascota;
+	let seguir;
+	let MascotaMasPesada;
+	let banderaPerroPesado;
+	let ultimoNombreMascotaOtraCosa;
+	let banderaSinPeloTempMenor;
+	let sumadorTemperaturaCorporalPerro;
+	let sumadorTemperaturaCorporalGato;
+	let sumadorTemperaturaCorporalOtraCosa;
+	let contadorPerro;
+	let contadorGato;
+	let contadorOtraCosa;
+	let sumadorGatosYperros;
+	let estadoEnfermo;
+	let estadoInternado;
+	let estadoAdopcion;
 
-	let repeticiones;
+
+	estadoEnfermo = 0;
+	estadoInternado = 0;
+	estadoAdopcion = 0;
+	sumadorGatosYperros = 0;
+	sumadorTemperaturaCorporalPerro = 0;
+	sumadorTemperaturaCorporalGato = 0;
+	sumadorTemperaturaCorporalOtraCosa = 0;
+	contadorPerro = 0;
+	contadorGato = 0;
+	contadorOtraCosa = 0;
+	banderaSinPeloTempMenor =0;
+	banderaPerroPesado = 0;
+	sumadorMascotas = 0;
+	sumadorMascotaEnfermas = 0;
+	seguir = "si";
+
+
+	while(seguir == "si")
+	{
+		tipoMascota = prompt("Ingrese el tipo de mascota: perro, gato u otra cosa");
+		while(isNaN(tipoMascota)==false || tipoMascota !="perro" && tipoMascota != "gato" && tipoMascota != "otra cosa")
+		{
+			tipoMascota = prompt("Error, ingrese el tipo de mascota: perro, gato u otra cosa");
+		}
+		tipoPelajeMascota = prompt("Ingrese el tipo de pelaje: corto, largo o sin pelo");
+		while(isNaN(tipoPelajeMascota)==false || tipoPelajeMascota != "corto" && tipoPelajeMascota != "largo" && tipoPelajeMascota != "sin pelo")
+		{
+			tipoPelajeMascota = prompt("Error, ingrese el tipo de pelaje: corto, largo o sin pelo");
+		}
+		edadMascota = prompt("Ingrese la edad de la mascota");
+		edadMascota = parseInt(edadMascota);
+		while(isNaN(edadMascota)==true || edadMascota <1 || edadMascota >30)
+		{
+			edadMascota = prompt("Error, ingrese la edad de la mascota");
+		}
+		nombreMascota = prompt("Ingrese el nombre de la mascota");
+		while(isNaN(nombreMascota)==false)
+		{
+			nombreMascota = prompt("Error, Ingrese el nombre de la mascota");
+		}
+		razaMascota = prompt("Ingrese la raza de la mascota");
+		while(isNaN(razaMascota)==false)
+		{
+			razaMascota = prompt("Error, ingrese la raza de la mascota")
+		}
+		pesoMascota = prompt("Ingrese el peso de la mascota");
+		pesoMascota = parseInt(pesoMascota);
+		while(isNaN(pesoMascota)==true || pesoMascota <1 || pesoMascota >100)
+		{
+			pesoMascota = prompt("Error, ingrese el peso de la mascota");
+			pesoMascota = parseInt(pesoMascota);
+		}
+		estadoclinicoMascota =prompt("Ingrese el estado clinico de la mascota: enfermo, internado o adopcion");
+		while(isNaN(estadoclinicoMascota)==false || estadoclinicoMascota != "enfermo" && estadoclinicoMascota != "internado" && estadoclinicoMascota != "adopcion")
+		{
+			estadoclinicoMascota =prompt("Error, Ingrese el estado clinico de la mascota");
+		}
+		temperaturaCorporalMascota = prompt("Ingrese la temperatura corporal de la mascota");
+		while(isNaN(temperaturaCorporalMascota)== true || temperaturaCorporalMascota <20 || temperaturaCorporalMascota > 45)
+		{
+			temperaturaCorporalMascota = prompt("Error, Ingrese la temperatura corporal de la mascota");
+		}
+
+		switch(tipoMascota)
+		{
+			case "perro":
+				contadorPerro++;
+				sumadorTemperaturaCorporalPerro= sumadorTemperaturaCorporalPerro + temperaturaCorporalMascota;
+				if(banderaPerroPesado==0 || pesoMascota > MascotaMasPesada)
+				{
+					MascotaMasPesado = pesoMascota;
+					banderaPerroPesado++;
+				}
+				if(estadoclinicoMascota=="enfermo")
+				{
+					sumadorMascotaEnfermas++;
+				}
+				break;
+			case "gato":
+				contadorGato++;
+				sumadorTemperaturaCorporalGato = sumadorTemperaturaCorporalGato + temperaturaCorporalMascota;
+				if(estadoclinicoMascota=="enfermo")
+				{
+					sumadorMascotaEnfermas++;
+				}
+				break;
+			case "otra cosa":
+				contadorOtraCosa++;
+				sumadorTemperaturaCorporalOtraCosa = sumadorTemperaturaCorporalOtraCosa + temperaturaCorporalMascota;
+				ultimoNombreMascotaOtraCosa = nombreMascota; //c)El nombre de la ultima mascota de tipo "otra cosa"
+				if(estadoclinicoMascota=="enfermo")
+				{
+					sumadorMascotaEnfermas++;
+				}
+				break;
+		}
+
+		// d)El animal sin pelo con menor temperatura corporal
+
+		if(tipoPelajeMascota=="sin pelo")
+		{
+			if(banderaSinPeloTempMenor==0 || temperaturaCorporalMascota<temperaturaCorporalMenorSinPelo)
+			{
+				temperaturaCorporalMenorSinPelo = temperaturaCorporalMascota;
+				banderaSinPeloTempMenor++;
+			}
+	
+		}
+
+		//g)Que estado clinico tiene la menor cantidad de mascotas Parte 1
+		switch(estadoclinicoMascota)
+		{
+			case "enfermo":
+				estadoEnfermo++;
+				break;
+			case "internado":
+				estadoInternado++;
+				break;
+			case "adopcion":
+				estadoAdopcion++;
+				break;
+		}
+
+		sumadorMascotas++;
+		seguir = prompt("Desea ingresar mas datos de mascotas? Ingrese si o no");
+	}//Fin del while
+
+
+	//e)Que tipo de mascota(gato o un perrro u "otra cosa") que tiene el mayor promedio de temperatura corporal
+
+	promedioTemeperaturaPerro = (sumadorTemperaturaCorporalPerro / contadorPerro);
+	promedioTemeperaturaGato = (sumadorTemperaturaCorporalGato / contadorGato);
+	promedioTemeperaturaOtraCosa = (sumadorTemperaturaCorporalOtraCosa / contadorOtraCosa);
+
+	if(promedioTemeperaturaPerro>promedioTemeperaturaGato && promedioTemeperaturaPerro>promedioTemeperaturaOtraCosa)
+	{
+		mayorPromedioTemperatura = "perro";
+	}
+	else
+	{
+		if(promedioTemeperaturaGato>promedioTemeperaturaOtraCosa)
+		{
+			mayorPromedioTemperatura = "gato";
+		}
+		else
+		{
+			mayorPromedioTemperatura = "otra cosa";
+		}
+	}
+
+
+	//f)Sumando gatos y perros que porcentaje del total de mascotas son?
+	sumadorGatosYperros = contadorGato + contadorPerro;
+	
+
+	if(sumadorGatosYperros>0)
+	{
+		porcentajeGatosYperros = (sumadorGatosYperros * 100) / sumadorMascotas;
+	}
+	
+	//g)Que estado clinico tiene la menor cantidad de mascotas Part 2
+	if(estado)
+	{
+		
+	}
+
+	/*g)Que estado clinico tiene la menor cantidad de mascotas
+	H)Cual es el promedio de kilos de peso de tomando todas las mascotas 
+	i)El nombre y raza del gato sin pelos mas liviano*/
+}//FIN DE LA FUNCIÓN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let repeticiones;
 	let contador;
 	
 	repeticiones = prompt("ingrese el número de repeticiones");
@@ -12,6 +248,4 @@ function mostrar()
 	{
 		console.log("Hola UTN FRA");
 	}
-
-
-}//FIN DE LA FUNCIÓN
+*/
