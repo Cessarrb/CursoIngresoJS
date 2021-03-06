f83e991f80327075fcbabe7d1e6e5b6e49f1b181
unknown <Cessarrb>
2021-03-06 06:55:09 -0300

    2021

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,14 +1,5 @@
-/*
-al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
+
 function mostrar()
 {
-	let claveIngresada;
-
-	claveIngresada = prompt("Ingrese el número clave.");
-
-	while(claveIngresada != "utn750")
-	{
-		claveIngresada = prompt("Clave incorrecta, vuelva a ingresar la clave");
-	}
-	alert("Clave correcta");
+	
 }//FIN DE LA FUNCIÓN
